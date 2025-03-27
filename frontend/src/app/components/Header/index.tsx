4b8867d091159b12e08d351f7b3fd628ff8819cf
Kinh Kha <khadoan.190809@gmail.com>
2025-03-27 15:43:27 +0700

    Update index.tsx

--- frontend/src/app/components/Header/index.tsx
+++ frontend/src/app/components/Header/index.tsx
@@ -132,6 +132,7 @@ export default function Header({ darkMode, toggleDarkMode }: HeaderProps) {
                         ))}
 
                         {/* Áo Dropdown */}
+                        <Box>
                             <Button
                                 onMouseEnter={handleAoMenuOpen}
                                 sx={{ 
