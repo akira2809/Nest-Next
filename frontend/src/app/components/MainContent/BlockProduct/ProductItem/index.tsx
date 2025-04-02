e46d66fc776c7f3f6e48ee970752ac693c73881c
Kinh Kha <khadoan.190809@gmail.com>
2025-04-02 15:18:01 +0700

    oof
    fix some issue

--- frontend/src/app/components/MainContent/BlockProduct/ProductItem/index.tsx
+++ frontend/src/app/components/MainContent/BlockProduct/ProductItem/index.tsx
@@ -157,7 +157,10 @@ export default function ProductItem({ product }: ProductItemProps) {
               ? "translate(-50%, -50%)"
               : "translate(-50%, 70%)",
             opacity: hovered ? 1 : 0,
-            transition: "all 0.3s ease",
+            transition: "all 0.4s ease",
+            zIndex: 2,
+            width: "70%",
+
           }}
         >
           <Button
